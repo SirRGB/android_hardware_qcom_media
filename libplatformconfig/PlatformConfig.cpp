7244b8a58d4f17584a9f900bda4c30f4322b1229
SirRGB <sirrgb@proton.me>
2025-10-13 20:16:17 +0200

    Revert "libplatformconfig: Fix KW issue"
    This reverts commit 84521b9ab3e3319af7e5a0eca4bf3d41cec2a3e4.

--- libplatformconfig/PlatformConfig.cpp
+++ libplatformconfig/PlatformConfig.cpp
@@ -112,7 +112,7 @@ ConfigError_t Config::getConfigStoreBool(const char *area, const char *config,
         return FAIL;
     }
 
-    CommandResult result = {Result::NOT_FOUND, 0};
+    CommandResult result;
     conf->mConfigStore->getConfig(area, config, [&](const CommandResult &res) {
         result = res;
     });
